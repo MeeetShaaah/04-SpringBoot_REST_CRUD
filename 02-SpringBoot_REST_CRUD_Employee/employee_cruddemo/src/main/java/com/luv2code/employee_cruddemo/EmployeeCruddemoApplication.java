4cfc9afb871690c2851065009f4419f5fe4f6f4a
Meet Shah <meetshah7199@gmail.com>
2023-11-20 16:25:55 -0500

    Move to New Folder and Add Employee_CRUD

---
+++ 02-SpringBoot_REST_CRUD_Employee/employee_cruddemo/src/main/java/com/luv2code/employee_cruddemo/EmployeeCruddemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.employee_cruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCruddemoApplication.class, args);
+	}
+
+}
---
+++ 02-SpringBoot_REST_CRUD_Employee/employee_cruddemo/src/test/java/com/luv2code/employee_cruddemo/EmployeeCruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.employee_cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
