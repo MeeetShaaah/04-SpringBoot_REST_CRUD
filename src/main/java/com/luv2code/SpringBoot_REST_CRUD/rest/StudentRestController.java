bcc18d3bc2b84d7db24287945274b72de6c45368
Meet Shah <meetshah7199@gmail.com>
2023-11-20 17:19:47 -0500

    Error Solving

---
+++ src/main/java/com/luv2code/SpringBoot_REST_CRUD/rest/StudentRestController.java
@@ -0,0 +1,61 @@
+package com.luv2code.SpringBoot_REST_CRUD.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.SpringBoot_REST_CRUD.entity.Student;
+
+import jakarta.annotation.PostConstruct;
+
+@RestController
+    @RequestMapping("/api")
+public class StudentRestController {
+    
+    List<Student> theStudents;
+    
+    // Define @PostConstructor to load the student data.
+
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
+        
+        theStudents.add(new Student("Meet", "Shah"));
+        theStudents.add(new Student("Kunj", "Shah"));
+        theStudents.add(new Student("Prexu", "Shah"));
+        theStudents.add(new Student("Shreya", "Shah"));
+        theStudents.add(new Student("Rajesh", "Shah"));
+    }
+
+    // Define the endpoint for "/students" return the list of students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        
+        // For now, not connecting any external database, can add here later.  
+        // For now, manually adding data to the student list and return them.
+
+        return theStudents;
+    }
+
+    //define endpoints for "/student/{studentID}"
+
+    @GetMapping("/students/{studentID}")
+    public Student getStudentByID(@PathVariable int studentID){
+        
+        //Just index of the list...for learning, later I will add studentID parameter from database...
+        //Check studentid with list size
+
+        if ((studentID >= theStudents.size()) || studentID < 0) {
+            throw new StudentNotFoundException("Student id not found:" + studentID  );
+        }
+        return theStudents.get(studentID);
+    }
+
+}
---
+++ src/test/java/com/luv2code/SpringBoot_REST_CRUD/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.SpringBoot_REST_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
